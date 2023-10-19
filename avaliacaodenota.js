e303f2d7f6bdea3b76a337341d6c96907c93a931
Carlos Emanuel <127751547+CarlosKarlos@users.noreply.github.com>
2023-10-19 13:37:32 -0300

    Update avaliacaodenota.js

--- avaliacaodenota.js
+++ avaliacaodenota.js
@@ -95,6 +95,28 @@ document.getElementById('avaliacao-form').addEventListener('submit', function (e
         return;
     }
 
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'atualizar_avaliacao.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+
+            // Atualize a interface da página com os dados retornados pelo servidor
+            atualizarMedia(response.media);
+
+            // Outras atualizações da interface, se necessário
+
+            // Marque o usuário como tendo avaliado
+            localStorage.setItem('avaliou', 'true');
+
+            // Recarregue a página, se necessário
+            window.location.reload();
+        }
+    };
+
+    xhr.send(`nota=${nota}`);
+
     // Recupera as avaliações do armazenamento local
     const avaliacoes = JSON.parse(localStorage.getItem('avaliacoes')) || [];
 
@@ -104,8 +126,7 @@ document.getElementById('avaliacao-form').addEventListener('submit', function (e
     // Salva as avaliações atualizadas no armazenamento local
     localStorage.setItem('avaliacoes', JSON.stringify(avaliacoes));
 
-    // Marque o usuário como tendo avaliado
-    localStorage.setItem('avaliou', 'true');
+    
 
     // Atualize a média e as barras como você já estava fazendo
     atualizarMedia();
@@ -141,6 +162,17 @@ function excluirUltimaAvaliacao() {
 }
 
 document.getElementById('reavaliar-btn').addEventListener('click', function () {
+    var novaNota = parseFloat(document.getElementById('nova-nota').value);
+
+    if (isNaN(novaNota) || novaNota < 0 || novaNota > 5) {
+        alert('Insira uma nova nota válida entre 0 e 5.');
+        return;
+    }
+
+    // Aqui você chama a função de AJAX para reavaliar o filme
+    var movieId = 'DragonballEvolution'; // Substitua pelo ID do filme atual
+    reavaliarFilme(movieId, novaNota);
+
     // Remova a marcação que indica que o usuário já avaliou
     localStorage.removeItem('avaliou');
 
@@ -148,9 +180,9 @@ document.getElementById('reavaliar-btn').addEventListener('click', function () {
     document.getElementById('nota').value = '';
 
     // Exiba ou ative o formulário de avaliação (dependendo de como você o escondeu anteriormente)
-    document.getElementById('avaliacao-form').style.display = 'block'; // ou 'inline', dependendo do estilo anterior
+    document.getElementById('avaliacao-form').style.display = 'block';
 
-    // Oculte ou desative o botão de reavaliação
+    // Oculte ou desative o botão de reavaliar
     this.style.display = 'none';
 
     // Redirecione para a mesma página para que o usuário possa reavaliar
@@ -159,13 +191,28 @@ document.getElementById('reavaliar-btn').addEventListener('click', function () {
     atualizarMedia();
     excluirUltimaAvaliacao();
 });
- // Verifique se o usuário já avaliou
-if (usuarioJaAvaliou()) {
-    // Se o usuário já avaliou, mostre o botão de reavaliar
-    document.getElementById('reavaliar-btn').style.display = 'block'; // ou 'inline', dependendo do estilo anterior
-} else {
-    // Se o usuário não avaliou, oculte o botão de reavaliar
-    document.getElementById('reavaliar-btn').style.display = 'none';
-}
-    atualizarBarras();
-    atualizarMedia();
+
+
+    function reavaliarFilme(movieId, novaNota) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'reavaliar_filme.php', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        
+        // Parâmetros da solicitação (movieId é o ID do filme, novaNota é a nova nota)
+        var params = 'filmeID=' + encodeURIComponent(movieId) + '&novaNota=' + encodeURIComponent(novaNota);
+        
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                // A solicitação foi concluída com sucesso, você pode atualizar a interface do usuário aqui
+                var response = JSON.parse(xhr.responseText);
+                if (response.reavaliado) {
+                    // O filme foi reavaliado com sucesso, atualize a interface de acordo
+                    // Por exemplo, atualize a média e a exibição da nota
+                } else {
+                    // Houve um erro ao reavaliar o filme, trate-o conforme necessário
+                }
+            }
+        };
+        
+        xhr.send(params);
+    }
