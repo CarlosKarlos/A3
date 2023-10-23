8c96023cdcc300b4a3f32a38f71a222096cee08e
Carlos Emanuel <127751547+CarlosKarlos@users.noreply.github.com>
2023-10-23 08:56:31 -0300

    Create favoriteCidadedosAnjos.js

---
+++ favoriteCidadedosAnjos.js
@@ -0,0 +1,41 @@
+function toggleFavorite(movieId) {
+    var favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    var index = favorites.indexOf(movieId);
+
+    if (index !== -1) {
+        favorites.splice(index, 1);
+        document.getElementById('favoriteIcon' + movieId).src = 'imagens/estrela_vazia.png';
+    } else {
+        favorites.push(movieId);
+        document.getElementById('favoriteIcon' + movieId).src = 'imagens/estrela_cheia.png';
+    }
+
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+    updateUI();
+}
+
+function initializeFavoriteIcons() {
+    var favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+
+    var movieIcons = {
+        CidadedosAnjos:'favoriteIconCidadedosAnjos',
+  
+        
+    };
+
+    for (var movieId in movieIcons) {
+        var favoriteIcon = document.getElementById(movieIcons[movieId]);
+
+        if (favorites.indexOf(movieId) !== -1) {
+            favoriteIcon.src = 'imagens/estrela_cheia.png';
+        } else {
+            favoriteIcon.src = 'imagens/estrela_vazia.png';
+        }
+    }
+}
+
+function updateUI() {
+    // Implemente a lógica para atualizar a interface do usuário conforme necessário
+}
+
+initializeFavoriteIcons();
